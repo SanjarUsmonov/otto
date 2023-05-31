2dacc880e8ac2e08cab2cd66c7685c8d6884bc52
SanjarUsmonov <108736016+SanjarUsmonov@users.noreply.github.com>
2023-05-31 22:59:50 +0500

    stop work

--- src/pages/tovar.jsx
+++ src/pages/tovar.jsx
@@ -4,8 +4,9 @@ const Tovar = () => {
   return <div className="tovar">
     <h1>Tovar qo'shish</h1>
     <div className="tovar_brand">
-        <label >brand name</label>
-        <input className="tovar_input" type="text" placeholder="Name"/>
+        <label >Brand nomi</label>
+        <input className="tovar_input" type="text" placeholder="Nomi"/>
+        <input className="tovar_input_file" type="file" />
     </div>
   </div>;
 };
